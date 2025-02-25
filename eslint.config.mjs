ceed21c4126c651f091023a774d706f9ef2119d0
marlonberaldo <marlon_beraldo@hotmail.com>
2025-02-25 10:43:06 -0300

    chore: add TailwindCSS plugin and custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,115 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:tailwindcss/recommended"),
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": ["warn"],
+      "eqeqeq": ["error", "always"],
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "no-multiple-empty-lines": ["error", { max: 1 }],
+      "sort-imports": ["error", {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: true
+      }],
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type"
+          ],
+          "pathGroups": [
+            {
+              "pattern": "react",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "next",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "next/**",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "@next/**",
+              "group": "builtin",
+              "position": "before"
+            },
+            {
+              "pattern": "@/components/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/_components/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "./**/components/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "./**/_components/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/lib/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/services/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/utils/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/queries/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/hooks/**",
+              "group": "internal",
+              "position": "before"
+            },
+            {
+              "pattern": "@/types/**",
+              "group": "internal",
+              "position": "before"
+            }
+          ],
+          "pathGroupsExcludedImportTypes": [
+            "builtin"
+          ],
+          "newlines-between": "always"
+        }
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
