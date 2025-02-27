8f6967ffadd43011b6065473ceff739ac5e916af
marlonberaldo <marlon_beraldo@hotmail.com>
2025-02-27 14:45:39 -0300

    feat: enhance CartList component with item removal functionality and improved layout

--- app/cart/_components/cart-list.tsx
+++ app/cart/_components/cart-list.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 
 import { useCart } from "@/context/cart-context";
 
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, Trash } from "lucide-react";
 
 const CartList = () => {
   const { cart, dispatch, loading } = useCart();
@@ -39,84 +39,121 @@ const CartList = () => {
     );
   }
 
+  const totalCart = cart.reduce((acc, item) => acc + item.quantity * (item.price - (item.discount || 0)), 0);
+
   return (
-    <ul className="space-y-2">
-      {cart.map((item) => (
-        <li key={item.id} className="flex flex-col items-start justify-between gap-5 rounded-lg border p-4 md:flex-row lg:items-center">
-          <figure className="rounded-lg bg-secondary p-2 ">
-            <Image
-              src={"/a50.webp"} // item.image is not available
-              alt={item.title}
-              width={150}
-              height={150}
-              className="object-contain"
-            />
-          </figure>
+    <section>
+      <ul className="space-y-4 pb-[50px]">
+        {cart.map((item) => (
+          <li key={item.id} className="mx-auto flex max-w-4xl flex-col items-start justify-between gap-4 rounded-lg border border-b-2 p-4 shadow md:flex-row md:items-center">
+            <div className="flex flex-col items-start gap-x-10 gap-y-4 md:flex-row md:items-center">
+              <Image
+                src={item.image || "/a50.webp"}
+                alt={item.title}
+                width={150}
+                height={150}
+                className="min-h-[150px] min-w-[150px] rounded-md object-contain md:border md:p-1"
+              />
 
-          <div className="flex max-w-full flex-col lg:max-w-[250px]">
-            <p className="line-clamp-2 text-lg font-bold">{item.title}</p>
-            <div className="flex items-center gap-2 text-lg font-semibold">
-              {item.discount > 0 ? (
-                <>
-                  <span className="font-bold text-green-600">
-                    {new Intl.NumberFormat("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    }).format(item.price - item.discount)}
-                  </span>
+              <div className="flex max-w-full flex-col md:max-w-[250px]">
+                <p className="line-clamp-2 text-lg font-bold">{item.title}</p>
 
-                  <span className="text-base text-muted-foreground/50 line-through">
-                    {new Intl.NumberFormat("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    }).format(item.price)}
-                  </span>
-                </>
-              ) : (
-                <span className="text-green-600">
-                  {new Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }).format(item.price)}
-                </span>
-              )}
+                <div className="flex items-start gap-x-2 font-semibold">
+                  {item.discount > 0 ? (
+                    <>
+                      <span className="font-bold text-green-600">
+                        {new Intl.NumberFormat("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        }).format(item.price - item.discount)}
+                      </span>
+
+                      <span className="text-base text-muted-foreground/50 line-through">
+                        {new Intl.NumberFormat("pt-BR", {
+                          style: "currency",
+                          currency: "BRL",
+                        }).format(item.price)}
+                      </span>
+                    </>
+                  ) : (
+                    <span className="text-green-600">
+                      {new Intl.NumberFormat("pt-BR", {
+                        style: "currency",
+                        currency: "BRL",
+                      }).format(item.price)}
+                    </span>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
 
-          <div className="flex items-center gap-4">
-            <Button
-              variant="secondary"
-              size="icon"
-              onClick={() =>
-                dispatch({ type: "UPDATE_QUANTITY", payload: { id: item.id, quantity: item.quantity - 1 } })
-              }
-            >
-              <Minus className="size-4" />
-            </Button>
+            <div className="flex flex-col items-start">
+              <span className="text-sm text-muted-foreground">Total ({item.quantity})</span>
+              <span className="font-semibold">
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item.quantity * (item.price - (item.discount || 0)))}
+              </span>
+            </div>
 
-            <span>{item.quantity}</span>
+            <div className="flex w-full flex-wrap items-start justify-between gap-2 md:w-auto md:flex-col lg:items-center">
+              <div className="flex items-center gap-4 rounded-lg border">
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  disabled={item.quantity === 1}
+                  onClick={() => dispatch({ type: "UPDATE_QUANTITY", payload: { id: item.id, quantity: item.quantity - 1 } })}
+                >
+                  <Minus className="size-4" />
+                </Button>
 
-            <Button
-              variant="secondary"
-              size="icon"
-              onClick={() =>
-                dispatch({ type: "UPDATE_QUANTITY", payload: { id: item.id, quantity: item.quantity + 1 } })
-              }
-            >
-              <Plus className="size-4" />
-            </Button>
+                <span>{item.quantity}</span>
+
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => dispatch({ type: "UPDATE_QUANTITY", payload: { id: item.id, quantity: item.quantity + 1 } })}
+                >
+                  <Plus className="size-4" />
+                </Button>
+              </div>
+
+              <Button
+                variant="destructive"
+                onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: item.id })}
+              >
+                <Trash className="size-4" /> Remover
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <div className="fixed bottom-0 left-0 z-40 w-full gap-4 border-t bg-background px-4 py-3">
+        <div className="mx-auto flex max-w-4xl flex-col items-center justify-center md:flex-row md:justify-between">
+          <div className="flex w-full items-center justify-between gap-x-4 md:w-auto">
+            <span className="font-semibold text-muted-foreground">Total</span>
+            <span className="font-bold underline">
+              {new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" }).format(totalCart)}
+            </span>
           </div>
 
           <Button
-            variant="destructive"
-            className="w-full max-w-[150px]"
-            onClick={() => dispatch({ type: "REMOVE_FROM_CART", payload: item.id })}
+            variant="default"
+            size="default"
+            className="mt-3 w-full bg-primary text-white md:mt-0 md:w-[250px]"
+            aria-label="Finalizar compra"
+            onClick={() => {
+              alert("Parabéns! Sua compra foi finalizada com sucesso!");
+              dispatch({ type: "CLEAR_CART" });
+            }}
           >
-            Remover
+            Finalizar compra
           </Button>
-        </li>
-      ))}
-    </ul>
+        </div>
+      </div>
+    </section>
   );
 };
 
